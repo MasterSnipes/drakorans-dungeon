1d96aaec641214a89ab05f421b06974cf1c58b13
BuildTools <unconfigured@null.spigotmc.org>
2016-10-16 11:50:10 -0400

    typewriter volume + some formatting

--- js/app.js
+++ js/app.js
@@ -4,7 +4,8 @@ var audioSources = [];
 var app = new Vue({
   el: '#game',
   data: {
-    storyText: 'Welcome to Drakoran\'s Dungeon'
+    storyText: 'Welcome to Drakoran\'s Dungeon',
+    placeholder: 'This is a butt'
   }
 });
 
@@ -12,11 +13,13 @@ var audio = [];
 function initAudio(){
 	for(i = 1; i <= 12; i++){
 		audio[(i-1)] = new Audio();
-		audio[(i-1)].src='audio/typewriter' + i + '.wav';
+		audio[(i-1)].src='audio/typewriter' + i + '.mp3';
+		audio[(i-1)].volume = 1;
 		console.log(audio[(i-1)].src);
 	}
 }
-document.getElementById('#music').volume = 0.2;
+document.getElementById('music').volume = 0.4;
+
 $( document ).keypress(function(keycode) {
 	if( keycode.which == 13) console.log('Enter');
 	else audio[Math.floor(Math.random() * 12)].play();
