e9f3b287cd395de67366170aaef09915cd9b799d
BuildTools <unconfigured@null.spigotmc.org>
2016-10-16 09:00:17 -0400

    added audio stuff

--- js/app.js
+++ js/app.js
@@ -1,6 +1,24 @@
+var num;
+var audioSources = [];
+
 var app = new Vue({
   el: '#game',
   data: {
     storyText: 'Welcome to Drakoran\'s Dungeon'
   }
-})
+});
+
+var audio = [];
+function initAudio(){
+	for(i = 1; i <= 12; i++){
+		audio[(i-1)] = new Audio();
+		audio[(i-1)].src='audio/typewriter' + i + '.wav';
+		console.log(audio[(i-1)].src);
+	}
+}
+document.getElementById('#music').volume = 0.2;
+$( document ).keypress(function(keycode) {
+	if( keycode.which == 13) console.log('Enter');
+	else audio[Math.floor(Math.random() * 12)].play();
+});
+initAudio();
